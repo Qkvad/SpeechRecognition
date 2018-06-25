11255e7817bdaf73c55f832fe2cfed89010532ab
Petra Brčić <petrabrcic94@gmail.com>
2018-06-25 14:06:54 +0200

    ADD: doc/ and octave/

---
+++ octave/get_spectrogram.m
@@ -0,0 +1,19 @@
+function get_spectrogram
+  % fs...audio rate ; y...audio data
+  [y,fs]=audioread('ab00c4b2_nohash_0.wav'); 
+  figure(1)
+  plot(y,'b-')
+  
+  figure(2)
+  step=fix(10*fs/1000); %one spectral slice every 10ms
+  window=fix(80*fs/1000); %40ms data window
+  fftn=2^nextpow2(window); 
+  [S,f,t]=specgram(y,fftn,fs,window,window-step);
+  S=abs(S(2:fftn*4000/fs,:));
+  S=S/max(S(:));
+  S=max(S,10^(-40/10));
+  S=min(S,10^(-3/10));
+  imagesc(flipud(log(S)));
+  %imagesc(t,f,log(S));
+  %set(gca,"ydir","normal");
+end
