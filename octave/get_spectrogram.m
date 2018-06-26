7c76d32f3ed845f5e1c0e679787fbd74b0395a42
Petra Brčić <petrabrcic94@gmail.com>
2018-06-26 18:06:47 +0200

    ADD: octave/get-spectrogram labels, pkg, specgram

--- octave/get_spectrogram.m
+++ octave/get_spectrogram.m
@@ -1,8 +1,12 @@
 function get_spectrogram
+  pkg load signal
   % fs...audio rate ; y...audio data
   [y,fs]=audioread('ab00c4b2_nohash_0.wav'); 
   figure(1)
   plot(y,'b-')
+  axis([0 16000 -0.8 0.8])
+  xlabel('time')
+  ylabel('sound preasure')
   
   figure(2)
   step=fix(10*fs/1000); %one spectral slice every 10ms
@@ -13,7 +17,7 @@ function get_spectrogram
   S=S/max(S(:));
   S=max(S,10^(-40/10));
   S=min(S,10^(-3/10));
-  imagesc(flipud(log(S)));
-  %imagesc(t,f,log(S));
-  %set(gca,"ydir","normal");
+  %imagesc(flipud(log(S)));
+  imagesc(t,f,log(S));
+  set(gca,"ydir","normal");
 end
